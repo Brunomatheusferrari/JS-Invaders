dfa737871940ab34587ffd6fc85ae068e1b2b713
Brunomatheusferrari <brunomatheussferrari@gmail.com>
2021-08-21 14:09:41 -0300

    Base Game Finalizada

--- static/js/gameJs.js
+++ static/js/gameJs.js
@@ -3,20 +3,25 @@ var velT;
 var tamTelaW, tamTelaH;
 var jogo;
 var frames;
+var contInimigos, painelContInimigos, velI, tmpCriaInimigo;
+var inimigosTotal;
+var vidaPlaneta, barraPlaneta;
+var telaWin = document.querySelector(".victoryScreenBg");
+var telaLoose = document.querySelector(".looseScreenBg");
 
 function teclaDw() {
     var tecla = event.keyCode;
-    if(tecla == 38){//cima
+    if (tecla == 38) {//cima
         diryJ = -1;
-    }else if(tecla == 40){//baixo
+    } else if (tecla == 40) {//baixo
         diryJ = 1;
     }
-    if(tecla == 37){//esquerda
+    if (tecla == 37) {//esquerda
         dirxJ = -1;
-    }else if(tecla == 39){//direita
+    } else if (tecla == 39) {//direita
         dirxJ = 1;
     }
-    if(tecla == 32){//espaço//tiro
+    if (tecla == 32) {//espaço//tiro
         //TIRO
         atira(pjx + 55, pjy + 90);
         atira(pjx + 135, pjy + 90);
@@ -25,78 +30,160 @@ function teclaDw() {
 
 function teclaUp() {
     var tecla = event.keyCode;
-    if((tecla == 38)||(tecla == 40)){//cima
+    if ((tecla == 38) || (tecla == 40)) {//cima
         diryJ = 0;
     }
-    if((tecla == 37)||(tecla == 39)){//esquerda
+    if ((tecla == 37) || (tecla == 39)) {//esquerda
         dirxJ = 0;
     }
 }
 
-function atira(x,y) {
-    var t = document.createElement("div");
-    var att1 = document.createAttribute("class");
-    var att2 = document.createAttribute("style");
-    // var att3 = document.createAttribute("src");
-    att1.value = "tiroJog";
-    att2.value = "top:" + y + "px;left:" + x + "px";
-    t.setAttributeNode(att1);
-    t.setAttributeNode(att2);
-    document.body.appendChild(t);
+function criaInimigo() {
+    if (jogo) {
+        var y = 0;
+        var x = Math.random() * tamTelaW;
+        var inimigo = document.createElement("div");
+        var att1 = document.createAttribute("class");
+        var att2 = document.createAttribute("style");
+        att1.value = "inimigo";
+        att2.value = "top:" + y + "px;left:" + x + "px;";
+        inimigo.setAttributeNode(att1);
+        inimigo.setAttributeNode(att2);
+        document.body.appendChild(inimigo);
+        contInimigos--;
+    }
 }
 
-function controleTiro(){
-    var tiros = document.getElementsByClassName("tiroJog");
-    var tam = tiros.length;
-    for(var i = 0; i < tam; i++){
-        if(tiros[i]){
-            var pt = tiros[i].offsetTop;
-            pt -= velT;
-            tiros[i].style.top = pt + "px";
-
-            if(pt < 0) {
-                tiros[i].remove();
+function controlaInimigo() {
+    inimigosTotal = document.getElementsByClassName("inimigo");
+    var tam = inimigosTotal.length;
+    for (var i = 0; i < tam; i++) {
+        if (inimigosTotal[i]) {
+            var pi = inimigosTotal[i].offsetTop;
+            pi += velI;
+            inimigosTotal[i].style.top =  pi + "px";
+            if (pi > tamTelaH) {
+                vidaPlaneta-=10;
+                inimigosTotal[i].remove();
             }
         }
     }
 }
 
-function controlaJogador(){
-    pjy += diryJ*velJ;
-    pjx += dirxJ*velJ;
-    jog.style.top = pjy + "px";
-    jog.style.left = pjx + "px";
-}
+        function atira(x, y) {
+            var t = document.createElement("div");
+            var att1 = document.createAttribute("class");
+            var att2 = document.createAttribute("style");
+            // var att3 = document.createAttribute("src");
+            att1.value = "tiroJog";
+            att2.value = "top:" + y + "px;left:" + x + "px";
+            t.setAttributeNode(att1);
+            t.setAttributeNode(att2);
+            document.body.appendChild(t);
+        }
 
-function gameLoop(){
-    if(jogo) {
-        //FUNÇÕES DE CONTROLE
-        controlaJogador();
-        controleTiro()
-    }
-    frames = requestAnimationFrame(gameLoop);
-}
+        function controleTiro() {
+            var tiros = document.getElementsByClassName("tiroJog");
+            var tam = tiros.length;
+            for (var i = 0; i < tam; i++) {
+                if (tiros[i]) {
+                    var pt = tiros[i].offsetTop;
+                    pt -= velT;
+                    tiros[i].style.top = pt + "px";
+                    colisaoTiroInimigo(tiros[i])
+                    if (pt < 0) {
+                        tiros[i].remove();
+                    }
+                }
+            }
+        }
 
-function inicia(){
-    jogo = true;
+        function colisaoTiroInimigo(tiro) {
+            var tam = inimigosTotal.length;
+            for(var i = 0; i < tam; i++) {
+                if(inimigosTotal[i]) {
+                    if(
+                        (
+                            (tiro.offsetTop <= (inimigosTotal[i].offsetTop+60))&& //Cima tiro com parte de baixo do inimigo
+                            ((tiro.offsetTop+6) >= (inimigosTotal[i].offsetTop)) ///Baixo tiro com cima inimigo
+                        )
+                        &&
+                        (
+                            (tiro.offsetLeft <= (inimigosTotal[i].offsetLeft+60))&&
+                            ((tiro.offsetLeft+6) >= (inimigosTotal[i].offsetLeft))
+                        )
+                    ){
+                        inimigosTotal[i].remove();
+                        tiro.remove();
+                    }
+                }
+            }
+        }
 
-    //ini Tela
-    tamTelaH = window.innerHeight;
-    tamTelaW = window.innerWidth;
+        function controlaJogador() {
+            pjy += diryJ * velJ;
+            pjx += dirxJ * velJ;
+            jog.style.top = pjy + "px";
+            jog.style.left = pjx + "px";
+        }
 
-    //ini Jog
-    dirxJ = diryJ = 0;
-    pjx = tamTelaW/2;
-    pjy = tamTelaH/2;
-    velJ = 15;
-    velT = 10;
-    jog = document.querySelector(".nave");
-    jog.style.top = pjy + "px";
-    jog.style.left = pjx + "px";
+        function gerenciaGame(){
+            barraPlaneta.style.width = vidaPlaneta + "px";
+            if(contInimigos <= 0) {
+                jogo=false;
+                clearInterval(tmpCriaInimigo);
+                telaWin.style.display = "flex"
+            }
 
-    gameLoop();
-}
+            if(vidaPlaneta <= 0) {
+                jogo=false;
+                clearInterval(tmpCriaInimigo);
+                telaLoose.style.display = "flex"
+            }
+        }
 
-window.addEventListener("load", inicia);
-document.addEventListener("keydown", teclaDw);
-document.addEventListener("keyup", teclaUp);
+        function gameLoop() {
+            if (jogo) {
+                //FUNÇÕES DE CONTROLE
+                controlaJogador();
+                controleTiro();
+                controlaInimigo();
+            }
+            gerenciaGame()
+            frames = requestAnimationFrame(gameLoop);
+        }
+
+        function inicia() {
+            jogo = true;
+
+            //ini Tela
+            tamTelaH = window.innerHeight;
+            tamTelaW = window.innerWidth;
+
+            //ini Jog
+            dirxJ = diryJ = 0;
+            pjx = tamTelaW / 2;
+            pjy = tamTelaH / 2;
+            velJ = 15;
+            velT = 10;
+            jog = document.querySelector(".nave");
+            jog.style.top = pjy + "px";
+            jog.style.left = pjx + "px";
+
+            //Controle dos Inimigos
+            clearInterval(tmpCriaInimigo);
+            contInimigos = 100;
+            velI = 5;
+            tmpCriaInimigo = setInterval(criaInimigo, 1700);
+
+            //Controles Planeta
+            vidaPlaneta = 100;
+            barraPlaneta = document.querySelector(".vidaPlaneta");
+            barraPlaneta.style.width = vidaPlaneta + "px";
+
+            gameLoop();
+        }
+
+        window.addEventListener("load", inicia);
+        document.addEventListener("keydown", teclaDw);
+        document.addEventListener("keyup", teclaUp);
